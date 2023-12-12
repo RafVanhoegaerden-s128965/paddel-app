af2598b80828328d5f95bb79c2acd90c5a710a53
Bayram Han Capa <bayramcapa85@gmail.com>
2023-12-12 16:47:58 +0100

    Added cards to book court fragment

--- app/src/main/java/com/example/paddel_app/ui/book_court/BookCourtFragment.kt
+++ app/src/main/java/com/example/paddel_app/ui/book_court/BookCourtFragment.kt
@@ -2,6 +2,7 @@ package com.example.paddel_app.ui.book_court
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.MenuItem
@@ -9,19 +10,23 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.paddel_app.MainActivity
 import com.example.paddel_app.R
 import com.example.paddel_app.databinding.FragmentBookCourtBinding
 import com.example.paddel_app.databinding.FragmentPlayBinding
+import com.example.paddel_app.ui.play.CustomAdapter
 import com.example.paddel_app.ui.play.PlayViewModel
 
 class BookCourtFragment : Fragment() {
 
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: CustomAdapter
     private var _binding: FragmentBookCourtBinding? = null
     private lateinit var bookCourtViewModel: BookCourtViewModel
-
-    private lateinit var clubSpinner: Spinner
 
     private val binding get() = _binding!!
 
@@ -32,6 +37,8 @@ class BookCourtFragment : Fragment() {
         _binding = FragmentBookCourtBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        bookCourtViewModel = ViewModelProvider(this).get(BookCourtViewModel::class.java)
+
         //region Up-Button
         // Go back to original fragment logic
         val activity = requireActivity() as AppCompatActivity
@@ -39,10 +46,25 @@ class BookCourtFragment : Fragment() {
         setHasOptionsMenu(true)
         //endregion
 
+        recyclerView = binding.recyclerView
+        adapter = CustomAdapter()
+
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = adapter
+
         // Load Courts
         (activity as? MainActivity)?.getCourts { courts ->
-            //playViewModel.setCourtsList(courts)
+            bookCourtViewModel.setCourtsList(courts)
         }
+
+        // Observe changes in the courts list
+        bookCourtViewModel.getCourtsList().observe(viewLifecycleOwner, Observer { courtsList ->
+            // Update the UI with the new list of courts
+            adapter.submitList(courtsList)
+            for (court in courtsList) {
+                Log.d("BookCourtFragment", "Court: ${court}")
+            }
+        })
 
 
 
--- app/src/main/java/com/example/paddel_app/ui/book_court/BookCourtViewModel.kt
+++ app/src/main/java/com/example/paddel_app/ui/book_court/BookCourtViewModel.kt
@@ -1,7 +1,20 @@
 package com.example.paddel_app.ui.book_court
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.paddel_app.model.Court
 
 class BookCourtViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val _courtsList = MutableLiveData<List<Court>>()
+
+    fun setCourtsList(courts: List<Court>) {
+        Log.d("BookCourtViewModel", "Court: ${_courtsList.value}")
+        _courtsList.value = courts
+    }
+
+    fun getCourtsList(): LiveData<List<Court>> {
+        return _courtsList
+    }
 }
