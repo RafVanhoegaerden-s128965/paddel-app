34b40954572f2cde85777ded2e964e8a8f210e1a
Bayram Han Capa <bayramcapa85@gmail.com>
2023-12-20 21:56:31 +0100

    Added dropdown on signup for gender

--- app/src/main/java/com/example/paddel_app/SignUpActivity.kt
+++ app/src/main/java/com/example/paddel_app/SignUpActivity.kt
@@ -1,39 +1,50 @@
 package com.example.paddel_app
-
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.auth.FirebaseAuth
+import com.example.paddel_app.LoginActivity
+import com.example.paddel_app.R
 import com.example.paddel_app.databinding.ActivitySignUpBinding
 import com.example.paddel_app.model.User
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 
 class SignUpActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
-
     private lateinit var binding: ActivitySignUpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
-
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         auth = FirebaseAuth.getInstance()
 
+        // Set up gender spinner
+        val genderSpinner = binding.spinnerGender
+        val genderAdapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.gender_options,
+            android.R.layout.simple_spinner_item
+        )
+        genderAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        genderSpinner.adapter = genderAdapter
+
         binding.btnSignUp.setOnClickListener {
             val firstName = binding.etFirstName.text.toString()
             val lastName = binding.etLastName.text.toString()
             val birthDate = binding.etDOB.text.toString()
-            val gender = binding.etGender.text.toString()
+            val gender = genderSpinner.selectedItem.toString()
             val email = binding.etEmail.text.toString()
             val password = binding.etPassword.text.toString()
 
-
-            if (firstName.isNotEmpty() && lastName.isNotEmpty() && gender.isNotEmpty() && birthDate.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty()) {
+            if (firstName.isNotEmpty() && lastName.isNotEmpty() && gender.isNotEmpty()
+                && birthDate.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty()
+            ) {
                 auth.createUserWithEmailAndPassword(email, password)
                     .addOnCompleteListener(this) { task ->
                         if (task.isSuccessful) {
@@ -47,7 +58,9 @@ class SignUpActivity : AppCompatActivity() {
                             val userId = user!!.uid
 
                             val newUser = User(
-                                userId,firstName, lastName, email, gender, birthDate)
+                                userId, firstName, lastName, email, gender, birthDate
+                            )
+
                             // Set user fields
                             val userData = hashMapOf(
                                 "firstName" to newUser.firstName,
@@ -55,10 +68,7 @@ class SignUpActivity : AppCompatActivity() {
                                 "email" to newUser.email,
                                 "gender" to newUser.gender,
                                 "birthDate" to newUser.birthDate,
-                                "bestHand" to newUser.bestHand,
-                                "courtPosition" to newUser.courtPosition,
-                                "matchType" to newUser.matchType,
-                                "preferredTime" to newUser.preferredTime,
+                                // Add other fields if needed
                             )
 
                             // Add user data to Firestore userID as documentID
@@ -74,20 +84,30 @@ class SignUpActivity : AppCompatActivity() {
                                 .addOnFailureListener { e ->
                                     // Handle errors
                                     Log.e("Firestore", "Error adding user data", e)
-                                    Toast.makeText(this, "Error: Registration failed.", Toast.LENGTH_SHORT).show()
+                                    Toast.makeText(
+                                        this,
+                                        "Error: Registration failed.",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
                                 }
 
                         } else {
-                            if (password.length < 6){
-                                Toast.makeText(this, "Password must be atleast 6 characters.", Toast.LENGTH_SHORT).show()
+                            if (password.length < 6) {
+                                Toast.makeText(
+                                    this,
+                                    "Password must be at least 6 characters.",
+                                    Toast.LENGTH_SHORT
+                                ).show()
                             }
-                            Toast.makeText(this, "Registration failed. Try again.", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(this, "Registration failed. Try again.", Toast.LENGTH_SHORT)
+                                .show()
                         }
                     }
             } else {
                 Toast.makeText(this, "Fill out all fields.", Toast.LENGTH_SHORT).show()
             }
         }
+
         binding.btnBackToLogin.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
